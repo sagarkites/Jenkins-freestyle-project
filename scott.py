dfbd65d06e8f663f9cddae25fd0a01b8bc202b61
vidyasagar <33413645+sagarkites@users.noreply.github.com>
2019-07-24 13:20:49 +0530

    Update and rename scott.py to app.py

--- scott.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def sagar():
-    return 'Welcome this is sagar currently testing Docker'
-
-if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0')
